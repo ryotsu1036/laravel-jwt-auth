a1dc7cabcd09403797650a6ae480d23b764a1663
JiaFeng <jiafeng@JiaFengdeMacBook-Air.local>
2020-02-16 23:13:56 +0800

    Initial commit

---
+++ resources/js/pages/admin/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>{{ count }}</p>
+    <p>
+      <v-btn fab dark small color="primary" @click="decrement">
+        <v-icon dark>mdi-minus</v-icon>
+      </v-btn>
+
+      <v-btn fab dark small color="primary" @click="increment">
+        <v-icon dark>mdi-plus</v-icon>
+      </v-btn>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    count () {
+      return this.$store.state.count;
+    }
+  },
+  methods: {
+    increment () {
+      this.$store.commit('increment');
+    },
+    decrement () {
+      this.$store.commit('decrement');
+    }
+  }
+}
+</script>
---
+++ resources/js/routes.js
@@ -0,0 +1,35 @@
+import AppLayout from './pages/admin/layouts/App';
+import Login from './pages/auth/Login';
+import Home from './pages/admin/Home';
+import About from './pages/admin/About';
+
+export default {
+  mode: 'history',
+
+  routes: [
+    {
+      path: '*',
+      redirect: { name: 'admin' },
+    },
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      name: 'admin',
+      path: '/admin',
+      component: AppLayout,
+      meta: { requiresAuth: true },
+      children: [
+        {
+          path: 'home',
+          component: Home
+        },
+        {
+          path: 'about',
+          component: About
+        }
+      ]
+    }
+  ]
+}
